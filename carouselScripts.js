1b3c32c4a66d4f7d4c3c2340311054fe5f53615b
unknown <vishwashkamal2000@gmail.com>
2023-11-25 08:44:06 -0800

    provent section

--- carouselScripts.js
+++ carouselScripts.js
@@ -1,62 +1,64 @@
-const images = [
-  "/assets/img/mobie_slider.png",
-  "/assets/img/mobie_slider 2.png",
-  "/assets/img/mobie_slider.png",
-  "/assets/img/mobie_slider 2.png",
-  "/assets/img/mobie_slider.png",
-  "/assets/img/mobie_slider 2.png",
-];
+// const images = [
+//   "/assets/img/mobie_slider.png",
+//   "/assets/img/mobie_slider 2.png",
+//   "/assets/img/mobie_slider.png",
+//   "/assets/img/mobie_slider 2.png",
+//   "/assets/img/mobie_slider.png",
+//   "/assets/img/mobie_slider 2.png",
+// ];
 
-const contentDescriptions = [
-  "Radio FM",
-  "Another description",
-  "Yet another description",
-  "And so on...",
-  "Description 5",
-  "Last description"
-];
+// const contentDescriptions = [
+//   "Radio FM",
+//   "Another description",
+//   "Yet another description",
+//   "And so on...",
+//   "Description 5",
+//   "Last description"
+// ];
 
-let currentImageIndex = 0;
-const displayedImg = document.getElementById("displayedImg");
-const currentSlideSpan = document.getElementById("currentSlide");
-const totalSlidesSpan = document.getElementById("totalSlides");
-const imageDescription = document.getElementById("imageDescription");
-const downloadCount = document.getElementById("downloadCount");
+// let currentImageIndex = 0;
+// const displayedImg = document.getElementById("displayedImg");
+// const currentSlideSpan = document.getElementById("currentSlide");
+// const totalSlidesSpan = document.getElementById("totalSlides");
+// const imageDescription = document.getElementById("imageDescription");
+// const downloadCount = document.getElementById("downloadCount");
 
-totalSlidesSpan.textContent = images.length.toString().padStart(2, '0');
+// totalSlidesSpan.textContent = images.length.toString().padStart(2, '0');
 
-document.getElementById("left_btn").addEventListener("click", function () {
-  if (currentImageIndex > 0) {
-      currentImageIndex--;
-  } else {
-      currentImageIndex = images.length - 1;
-  }
+// document.getElementById("left_btn").addEventListener("click", function () {
+//   if (currentImageIndex > 0) {
+//       currentImageIndex--;
+//   } else {
+//       currentImageIndex = images.length - 1;
+//   }
 
-  updateDisplayedContent();
-});
+//   updateDisplayedContent();
+// });
 
-document.getElementById("right_btn").addEventListener("click", function () {
-  if (currentImageIndex < images.length - 1) {
-      currentImageIndex++;
-  } else {
-      currentImageIndex = 0;
-  }
+// document.getElementById("right_btn").addEventListener("click", function () {
+//   if (currentImageIndex < images.length - 1) {
+//       currentImageIndex++;
+//   } else {
+//       currentImageIndex = 0;
+//   }
 
-  updateDisplayedContent();
-});
+//   updateDisplayedContent();
+// });
 
-function updateDisplayedContent() {
-  displayedImg.classList.add("slide-out");
+// function updateDisplayedContent() {
+//   displayedImg.style.transition = 'transform 0.3s ease-out';
+//   displayedImg.style.transform = 'translateX(100%)';
 
-  setTimeout(() => {
-      displayedImg.src = images[currentImageIndex];
-      imageDescription.textContent = contentDescriptions[currentImageIndex];
-      displayedImg.classList.remove("slide-out");
-  }, 300);
+//   setTimeout(() => {
+//       displayedImg.src = images[currentImageIndex];
+//       imageDescription.textContent = contentDescriptions[currentImageIndex];
+//       displayedImg.style.transition = 'transform 0.3s ease-out';
+//       displayedImg.style.transform = 'translateX(0)';
+//   }, 300);
 
-  const formattedCurrentSlide = (currentImageIndex + 1).toString().padStart(2, "0");
-  currentSlideSpan.textContent = formattedCurrentSlide;
-}
+//   const formattedCurrentSlide = (currentImageIndex + 1).toString().padStart(2, "0");
+//   currentSlideSpan.textContent = formattedCurrentSlide;
+// }
 
-// Initial content display
-updateDisplayedContent();
+// // Initial content display
+// updateDisplayedContent();
